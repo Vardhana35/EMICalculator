8061afbc3e901ba4337814eb9f54ede69ad38242
BOBBURI NAGA VARDHANA <126737264+Vardhana35@users.noreply.github.com>
2023-08-14 20:09:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import EMICalc from './components/EMICalc';
+
+function App() {
+  return (
+    <div className="App">
+      <EMICalc/>
+      </div>)
+}
+
+export default App;
